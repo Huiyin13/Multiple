da066b47e8defdbb14e712e08bd729dc45b95cbf
Huiyin13 <83337412+Huiyin13@users.noreply.github.com>
2021-12-21 21:40:06 +0800

    Initial commit

---
+++ app/src/main/java/com/example/multiple/MainActivity.java
@@ -0,0 +1,145 @@
+package com.example.multiple;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.OpenableColumns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.dynamic.IFragmentWrapper;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int IMAGE_CODE = 1;
+    Button selectBtn;
+    RecyclerView recyclerView;
+    List<ModalClass> modalClassList;
+    CustomAdapter customAdapter;
+
+    private StorageReference mStorageRef;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        selectBtn = findViewById(R.id.select_btn);
+        recyclerView = findViewById(R.id.recyclerViewId);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+
+        modalClassList = new ArrayList<>();
+
+        selectBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+                startActivityForResult(intent, IMAGE_CODE);
+
+
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+        if (requestCode == IMAGE_CODE && resultCode == RESULT_OK) {
+
+            if (data.getClipData() != null) {
+
+                int totalitem = data.getClipData().getItemCount();
+
+                for (int i = 0; i < totalitem; i++) {
+
+                    Uri imageUri = data.getClipData().getItemAt(i).getUri();
+                    String imagename = getFileName(imageUri);
+
+                    ModalClass modalClass = new ModalClass(imagename,imageUri);
+                    modalClassList.add(modalClass);
+
+                    customAdapter = new CustomAdapter(MainActivity.this, modalClassList);
+                    recyclerView.setAdapter(customAdapter);
+
+
+                    StorageReference mRef = mStorageRef.child("image").child(imagename);
+
+                    mRef.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                            Toast.makeText(MainActivity.this, "Done", Toast.LENGTH_SHORT).show();
+
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(MainActivity.this, "Fail" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+
+                }
+
+
+            } else if (data.getData() != null) {
+                Toast.makeText(this, "single", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+
+    }
+
+
+    public String getFileName(Uri uri) {
+        String result = null;
+        if (uri.getScheme().equals("content")) {
+            Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+            try {
+                if (cursor != null && cursor.moveToFirst()) {
+                    result = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+                }
+            } finally {
+                cursor.close();
+            }
+        }
+        if (result == null) {
+            result = uri.getPath();
+            int cut = result.lastIndexOf('/');
+            if (cut != -1) {
+                result = result.substring(cut + 1);
+            }
+        }
+        return result;
+    }
+
+
+}
